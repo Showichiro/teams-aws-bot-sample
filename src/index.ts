e31a5e48ff1e26bfeb52b64151c1be0f7a070c4c
Showichiro <show2305@gmail.com>
2021-05-22 16:59:14 +0900

    package.jsonの修正他

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { cloneDeep } from "lodash";
 import * as httpm from "typed-rest-client/HttpClient";
 import { IRequestOptions } from "typed-rest-client/Interfaces";
 
-export async function handler(event: any, context: any, callBack: any) {
+export async function handler(event: any) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   const body = JSON.parse(event.body) as Activity;
   const authorization = process.env.authorization;
   const conversationId = body.conversation.id;
   const activityId = body.id;
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   const uri = `v3/conversations/${conversationId}/activities/${activityId}`;
   const activity: Activity = cloneDeep(body);
   activity.from = cloneDeep(body.recipient);
